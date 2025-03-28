2bfc7659dbeb4035686a94a4b8cfc4ae3e1e4b05
Jotaulian <julianeggle@hotmail.com>
2025-03-28 11:38:26 +0100

    feat: Add guard to SELECT_CHARACTER event

--- src/navigationMachine.ts
+++ src/navigationMachine.ts
@@ -13,6 +13,10 @@ export const navigationMachine = setup({
   actors: {
     fetchCharacters: fromPromise(fetchCharactersPromise),
   },
+  guards: {
+    characterExists: ({ context, event }) =>
+      context.characters.some((c) => c.id === event.characterId),
+  },
 }).createMachine({
   /** @xstate-layout N4IgpgJg5mDOIC5QDsCGA3AllVAXTA9sgHQA2BqEmyUAxBEWMdegQNZMy4DCAFqgCdUAY1xgBsANoAGALqJQABwKxM+IgpAAPRAGYA7LuIAOXQFYAnAEZjxgGwAmK2el27AGhABPRA4d3iXQtXSwAWXTtpUP07XQBfOM80LBx1EnJKajpxAQIBYkVSPAAzPIBbYi4+QRExCRl5JBBlVTTNHQQDUOIXO1DjfStpV1c7M08fTrMzQLNnfQcLQYdpJYSkjGw8QhJeAjKwWgBlAFEAGRPuABUAfW4ACQBBACVH65Pnhs0WtR32xAWARcxjMjmM0n80n0FgmemIDn0lnBFgG-XMfXWIGSWzSxGE-CEonEABEwLhUJhSLQAHKPABqAEkAOKPK4nG73ADyAFkTl8mj82k0OnMZqEVrFQtJnA5dNFYQgrH5AvpQnYQRYzLoHNFQmYEoksQQIHBNNjUn8BSpfhphYgALT+fQmHVhKxWcUDUwK+3GbpROwWFYWXQRfzOTHm7ZEMgUKg0b7WoWgDqOqwWF16iyhd2e-Te7y+KImUKhFE50MRaS6YyRzYWmN7A6J1qWlMAsYmaRmBx+8OuGwKvzGeFlpZmMv9GJWfR1lLRkj4mpEgSk8mUls25D-BA96TEYYg6TGINQj36BXhQIWaz6O92eYPqwGuJAA */
   id: 'navigation',
@@ -42,10 +46,10 @@ export const navigationMachine = setup({
       on: {
         SELECT_CHARACTER: {
           target: 'characterDetail',
+          guard: 'characterExists',
           actions: assign({
             selectedCharacter: ({ context, event }) =>
-              context.characters.find((c) => c.id === event.characterId) ||
-              null,
+              context.characters.find((c) => c.id === event.characterId)!,
           }),
         },
       },
